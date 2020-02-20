309a053ab6300701e89dc4a12a074523c2d59508
MrBrenlla <47985849+MrBrenlla@users.noreply.github.com>
2020-02-20 21:12:59 +0100

    Add files via upload

--- p2/queue.c
+++ p2/queue.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
 
 // circular array
 typedef struct _queue {
@@ -6,18 +8,29 @@ typedef struct _queue {
     int used;
     int first;
     void **data;
+    pthread_mutex_t *m;
+    pthread_cond_t *cons;
+    pthread_cond_t *prod;
 } _queue;
 
 #include "queue.h"
 
 queue q_create(int size) {
     queue q = malloc(sizeof(_queue));
-    
+
     q->size  = size;
     q->used  = 0;
     q->first = 0;
     q->data  = malloc(size*sizeof(void *));
-    
+    for(int i=0; i<size;i++) q->data[i]=NULL;
+    q->m=malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(q->m,NULL);
+    q->cons=malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(q->cons,NULL);
+    q->prod=malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(q->prod,NULL);
+
+
     return q;
 }
 
@@ -26,27 +39,50 @@ int q_elements(queue q) {
 }
 
 int q_insert(queue q, void *elem) {
-    if(q->size == q->used) return 0;
 
-    q->data[(q->first+q->used) % q->size] = elem;    
+    pthread_mutex_lock(q->m);
+    while(q->used==q->size)
+      pthread_cond_wait(q->prod,q->m);
+    q->data[(q->first+q->used) % q->size] = elem;
     q->used++;
-    
+
+    printf("\n Insertado %d: \n[ ",*(int *)elem );
+    for (int i=0;i<q->used;i++)printf("%d ",*(int *)q->data[(q->first+i) % q->size] );
+    printf("]\n" );
+    if(q->used==1) pthread_cond_broadcast(q->cons);
+    pthread_mutex_unlock(q->m);
+
     return 1;
 }
 
 void *q_remove(queue q) {
     void *res;
-    if(q->used==0) return NULL;
-    
+
+    pthread_mutex_lock(q->m);
+    while(q->used==0)
+      pthread_cond_wait(q->cons,q->m);
+
     res = q->data[q->first];
-    
+    printf("\n Expulsado %d: \n[ ",*(int *)res);
+    for (int i=0;i<q->used;i++)printf("%d ",*(int *)q->data[(q->first+i) % q->size] );
+    printf("]\n" );
     q->first = (q->first+1) % q->size;
     q->used--;
-    
+
+    if(q->used==q->size-1) pthread_cond_broadcast(q->prod);
+    pthread_mutex_unlock(q->m);
+
     return res;
 }
 
 void q_destroy(queue q) {
+    for (int i=0;i<q->used;i++)printf("%d",*(int *)q->data[(q->first+i) % q->size] );
+    pthread_mutex_destroy(q->m);
+    free(q->m);
+    pthread_cond_destroy(q->cons);
+    free(q->cons);
+    pthread_cond_destroy(q->prod);
+    free(q->prod);
     free(q->data);
     free(q);
 }
