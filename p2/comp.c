cead94faebc9e62784a521c1ecbbd50402bc8c60
MrBrenlla <47985849+MrBrenlla@users.noreply.github.com>
2020-02-28 20:18:39 +0100

    Add files via upload

--- p2/comp.c
+++ p2/comp.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
+#include <pthread.h>
 #include "compress.h"
 #include "chunk_archive.h"
 #include "queue.h"
@@ -15,20 +16,108 @@
 #define QUEUE_SIZE 20
 
 #define COMPRESS 1
-#define DECOMPRESS 0
+#define DECOMPRESS 0;
+
+
+typedef struct{
+  queue in;
+  queue out;
+  chunk (*process)(chunk);
+  pthread_mutex_t* m;
+  int * iter;
+} args;
+
+typedef struct{
+  int chunks;
+  int size;
+  int fd;
+  queue q;
+  archive ar;
+} arge;
+
+void* lectorc(void* aux){
+  chunk ch;
+  int offset;
+  arge * arg=(arge*) aux;
+
+  for(int i=0; i<arg->chunks; i++) {
+      ch = alloc_chunk(arg->size);
+
+      offset=lseek(arg->fd, 0, SEEK_CUR);
+
+      ch->size   = read(arg->fd, ch->data, arg->size);
+      ch->num    = i;
+      ch->offset = offset;
+
+      q_insert(arg->q, ch);
+  }
+  printf("----------------Thread lector finalizado\n" );
+
+}
+
+void* escritorc(void* aux){
+  chunk ch;
+  arge * arg=(arge*) aux;
+
+  for(int i=0; i<arg->chunks; i++) {
+      ch = q_remove(arg->q);
+
+      add_chunk(arg->ar, ch);
+      free_chunk(ch);
+    }
+    printf("----------------Thread escritor finalizado\n" );
+
+}
+
+void* lectord(void* aux){
+  chunk ch;
+  arge * arg=(arge*) aux;
+
+  for(int i=0; i<chunks(arg->ar); i++) {
+      ch = get_chunk(arg->ar, i);
+      q_insert(arg->q, ch);
+  }
+  printf("----------------Thread lector finalizado\n" );
+
+}
+
+void* escritord(void* aux){
+  chunk ch;
+  arge * arg=(arge*) aux;
+
+  for(int i=0; i<chunks(arg->ar); i++) {
+      ch=q_remove(arg->q);
+      lseek(arg->fd, ch->offset, SEEK_SET);
+      write(arg->fd, ch->data, ch->size);
+      free_chunk(ch);
+  }
+  printf("----------------Thread escritor finalizado\n" );
+
+}
 
 
 // take chunks from queue in, run them through process (compress or decompress), send them to queue out
-void worker(queue in, queue out, chunk (*process)(chunk)) {
+void* worker(void* aux) {
+  args* args = aux;
     chunk ch, res;
-    while(q_elements(in)>0) {
-        ch = q_remove(in);
+    int * i = args->iter;
+    pthread_mutex_lock(args->m);
+    while(*i>0) {
+      (*i)=(*i)-1;
+      pthread_mutex_unlock(args->m);
 
-        res = process(ch);
-        free_chunk(ch);
+      ch = q_remove(args->in);
 
-        q_insert(out, res);
+      res = args->process(ch);
+      free_chunk(ch);
+
+      q_insert(args->out, res);
+
+      pthread_mutex_lock(args->m);
     }
+    pthread_mutex_unlock(args->m);
+    printf("----------------Thread finalizado\n" );
+    return NULL;
 }
 
 // Compress file taking chunks of opt.size from the input file,
@@ -63,33 +152,53 @@ void comp(struct options opt) {
     out = q_create(opt.queue_size);
 
     // read input file and send chunks to the in queue
-    for(i=0; i<chunks; i++) {
-        ch = alloc_chunk(opt.size);
+    pthread_t lector;
+    arge argl;
 
-        offset=lseek(fd, 0, SEEK_CUR);
+    argl.q=in;
+    argl.chunks=chunks;
+    argl.size=opt.size;
+    argl.fd=fd;
 
-        ch->size   = read(fd, ch->data, opt.size);
-        ch->num    = i;
-        ch->offset = offset;
-
-        q_insert(in, ch);
-    }
+    pthread_create(&lector,NULL,lectorc,(void*)&argl);
 
     // compression of chunks from in to out
-    worker(in, out, zcompress);
+    int p=chunks;;
+    args args;
+    pthread_t threads[opt.num_threads];
+    args.m=malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(args.m,NULL);
+    for(int i=0; i<opt.num_threads;i++) {
+    args.iter=&p;
+    args.in=in;
+    args.out=out;
+    args.process=zcompress;
+
+    pthread_create(&threads[i],NULL,worker,(void*)&args);}
+
+
 
     // send chunks to the output archive file
-    for(i=0; i<chunks; i++) {
-        ch = q_remove(out);
+    pthread_t escritor;
+    arge arge;
+    arge.chunks=chunks;
+    arge.q=out;
+    arge.ar=ar;
+    pthread_create(&escritor,NULL,escritorc,(void*)&arge);
 
-        add_chunk(ar, ch);
-        free_chunk(ch);
-    }
+
+
+
+    printf("----------------Finalizada creación de threads\n");
+
+    pthread_join(lector,NULL);
+    for(int i=0; i<opt.num_threads;i++) pthread_join(threads[i],NULL);
+    pthread_join(escritor,NULL);
 
     close_archive_file(ar);
     close(fd);
-    q_destroy(in);
-    q_destroy(out);
+     q_destroy(in);
+     q_destroy(out);
 }
 
 
@@ -125,22 +234,46 @@ void decomp(struct options opt) {
     in  = q_create(opt.queue_size);
     out = q_create(opt.queue_size);
 
+
     // read chunks with compressed data
-    for(i=0; i<chunks(ar); i++) {
-        ch = get_chunk(ar, i);
-        q_insert(in, ch);
-    }
+    pthread_t lector;
+    arge argl;
+    argl.q=in;
+    argl.ar=ar;
+
+    pthread_create(&lector,NULL,lectord,(void*)&argl);
 
     // decompress from in to out
-    worker(in, out, zdecompress);
+    int p=chunks(ar);;
+    args args;
+    pthread_t threads[opt.num_threads];
+    args.m=malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(args.m,NULL);
+    for(int i=0; i<opt.num_threads;i++) {
+    args.iter=&p;
+    args.in=in;
+    args.out=out;
+    args.process=zdecompress;
+
+    pthread_create(&threads[i],NULL,worker,(void*)&args);}
+
+
 
     // write chunks from output to decompressed file
-    for(i=0; i<chunks(ar); i++) {
-        ch=q_remove(out);
-        lseek(fd, ch->offset, SEEK_SET);
-        write(fd, ch->data, ch->size);
-        free_chunk(ch);
-    }
+    pthread_t escritor;
+    arge arge;
+
+    arge.q=out;
+    arge.ar=ar;
+    arge.fd=fd;
+
+    pthread_create(&escritor,NULL,escritord,(void*)&arge);
+
+    printf("----------------Finalizada creación de threads\n");
+
+    pthread_join(lector,NULL);
+    for(int i=0; i<opt.num_threads;i++) pthread_join(threads[i],NULL);
+    pthread_join(escritor,NULL);
 
     close_archive_file(ar);
     close(fd);
